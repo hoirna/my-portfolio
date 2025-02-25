5660b759ac7b39fabe8217864f29318a2bb548c1
hoirna <mrrhorna168@gmail.com>
2025-02-25 13:28:30 +0700

    resolve conflict

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
-import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ProjectsSection from '../components/ProjectsSection';
 import ContactSection from '../components/ContactSection';
-import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-purple-100">
-      <Navbar />
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
       <ContactSection />
-      <Footer />
     </div>
   );
 }
